250e069c767094d66488184d702786bac8bcb0ee
Niko Granö <niko@ironlions.fi>
2019-12-17 18:43:59 +0200

    Add margin to action buttons.

--- admin/src/views/stock/WidgetsActions.vue
+++ admin/src/views/stock/WidgetsActions.vue
@@ -1,16 +1,16 @@
 <template>
     <CRow>
         <CCol sm="6" lg="3">
-            <CButton size="lg" color="success" block>Add Stock</CButton>
+            <CButton class="stock-action-btn" size="lg" color="success" block>Add Stock</CButton>
         </CCol>
         <CCol sm="6" lg="3">
-            <CButton size="lg" color="info" block>Show Bellow Limit</CButton>
+            <CButton class="stock-action-btn" size="lg" color="info" block>Show Bellow Limit</CButton>
         </CCol>
         <CCol sm="6" lg="3">
-            <CButton size="lg" color="warning" block>Show Expiring</CButton>
+            <CButton class="stock-action-btn" size="lg" color="warning" block>Show Expiring</CButton>
         </CCol>
         <CCol sm="6" lg="3">
-            <CButton size="lg" color="danger" block>Show Expired</CButton>
+            <CButton class="stock-action-btn" size="lg" color="danger" block>Show Expired</CButton>
         </CCol>
     </CRow>
 </template>
@@ -20,3 +20,9 @@
         name: 'WidgetsStockActions'
     }
 </script>
+
+<style scoped>
+    .stock-action-btn {
+        margin-bottom: 10px;
+    }
+</style>
