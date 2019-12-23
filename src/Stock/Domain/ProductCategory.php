84f3d2449cdf7eae92e1519c5a7b5bf3d53728da
Niko Granö <niko@ironlions.fi>
2019-12-23 13:43:44 +0200

    Bypass for issue https://github.com/doctrine/common/issues/881. Revert, when solved.

--- src/Stock/Domain/ProductCategory.php
+++ src/Stock/Domain/ProductCategory.php
@@ -34,8 +34,9 @@ class ProductCategory
     private string $id;
     /**
      * @ORM\Column(type="string")
+     * @var string Please solve https://github.com/doctrine/common/issues/881 before adding strict type.
      */
-    public string $name;
+    public  $name;
     /**
      * @var ProductCollection[]|PersistentCollection
      * @ORM\OneToMany(targetEntity="ProductCollection", mappedBy="category", cascade={"persist", "remove"}, orphanRemoval=true)
