33c2641bfcc73d31fbe2605e43604eadcd2436ff
Niko Granö <niko@ironlions.fi>
2020-01-10 19:53:28 +0200

    Add Exception for failed request json body decoding.

--- src/Core/Domain/Shared/Exception/FailedToDecodeBodyException.php
+++ src/Core/Domain/Shared/Exception/FailedToDecodeBodyException.php
@@ -1,8 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ *
+ * Household 2020 — NOTICE OF LICENSE
+ * This source file is released under commercial license by copyright holders.
+ *
+ * @copyright 2017-2020 (c) Niko Granö (https://granö.fi)
+ * @copyright 2014-2020 (c) IronLions (https://ironlions.fi)
+ *
+ */
 
 namespace App\Core\Domain\Shared\Exception;
-
 
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,7 +20,7 @@ final class FailedToDecodeBodyException extends Exception
 {
     public function __construct(\JsonException $e)
     {
-        parent::__construct('Syntax error: '. $e->getMessage(), $e->getCode(), $e);
+        parent::__construct('Syntax error: '.$e->getMessage(), $e->getCode(), $e);
     }
 
     public function getResponse(string $body = '', int $status = 400, array $headers = []): Response
