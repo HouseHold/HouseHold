7bd2f2c2d3ee43436fc4efab0329faed0f3d7343
Niko Granö <niko@ironlions.fi>
2020-01-12 17:24:05 +0200

    Add fixes for MariaDB for Security module.

---
+++ src/Migrations/Security/Version20200112000001.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *
+ * Household 2020 — NOTICE OF LICENSE
+ * This source file is released under commercial license by copyright holders.
+ *
+ * @copyright 2017-2020 (c) Niko Granö (https://granö.fi)
+ * @copyright 2014-2020 (c) IronLions (https://ironlions.fi)
+ *
+ */
+
+namespace App\Migrations\Stock;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200112000001 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Fix MariaDB specific fields.';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE security_profile_audit CHANGE diffs diffs LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+        $this->addSql('ALTER TABLE security_users_audit CHANGE diffs diffs LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:json)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE security_profile_audit CHANGE diffs diffs LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_bin`');
+        $this->addSql('ALTER TABLE security_users_audit CHANGE diffs diffs LONGTEXT CHARACTER SET utf8mb4 DEFAULT NULL COLLATE `utf8mb4_bin`');
+    }
+}
