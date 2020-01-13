c1be59affeadce0f36ca10649c5b006e9b803ce7
Niko Granö <niko@ironlions.fi>
2020-01-13 19:55:16 +0200

    Add draft for removing quantity of products from the stock.

---
+++ src/Stock/Domain/ProductStock/Event/ProductConsumedStock.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *
+ * Household 2020 — NOTICE OF LICENSE
+ * This source file is released under commercial license by copyright holders.
+ *
+ * @copyright 2017-2020 (c) Niko Granö (https://granö.fi)
+ * @copyright 2014-2020 (c) IronLions (https://ironlions.fi)
+ *
+ */
+
+namespace App\Stock\Domain\ProductStock\Event;
+
+use Assert\Assertion;
+use Broadway\Serializer\Serializable;
+
+final class ProductConsumedStock implements Serializable
+{
+    public int $quantity;
+    public \DateTimeImmutable $bestBefore;
+
+    public function __construct(int $quantity, \DateTimeImmutable $bestBefore)
+    {
+        $this->quantity = $quantity;
+        $this->bestBefore = $bestBefore;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Exception
+     * @throws \Assert\AssertionFailedException
+     */
+    public static function deserialize(array $data): self
+    {
+        Assertion::keyExists($data, 'quantity');
+        Assertion::keyExists($data, 'bestBefore');
+
+        return new self($data['quantity'], new \DateTimeImmutable($data['bestBefore']));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function serialize(): array
+    {
+        return [
+            'quantity'   => $this->quantity,
+            'bestBefore' => $this->bestBefore,
+        ];
+    }
+}
