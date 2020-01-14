de5b159389efcd842b69d6795a2a0841864ae8a5
Niko Granö <niko@ironlions.fi>
2020-01-14 10:35:54 +0200

    Configure caching.

--- config/bundles.php
+++ config/bundles.php
@@ -21,4 +21,5 @@ return [
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     DH\DoctrineAuditBundle\DHDoctrineAuditBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
 ];
